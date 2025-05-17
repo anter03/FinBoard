ce6a8bf179993002b9d95715ab970151d3d5bc17
anter03 <64782750+anter03@users.noreply.github.com>
2025-05-17 10:28:44 +0200

    Add files via upload
    start

---
+++ FinBoard/BackEnd/src/main/java/pwork/greco/antonio/FinBoard/FinBoardApplication.java
@@ -0,0 +1,13 @@
+package pwork.greco.antonio.FinBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinBoardApplication.class, args);
+	}
+
+}
---
+++ FinBoard/BackEnd/src/test/java/pwork/greco/antonio/FinBoard/FinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package pwork.greco.antonio.FinBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
